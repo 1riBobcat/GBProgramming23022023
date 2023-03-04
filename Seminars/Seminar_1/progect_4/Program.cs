585828bd957e3c272ca0fd815052c239fdf4dbd2
1riBobcat <irisha.bobcat@gmail.com>
2023-03-04 17:42:45 +0300

    \New/

---
+++ Seminars/Seminar_1/progect_4/Program.cs
@@ -0,0 +1,22 @@
+﻿
+//Задача 8: Напишите программу, которая на вход принимает число (N), 
+//а на выходе показывает все чётные числа от 1 до N.
+
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+
+
+
+Console.Write("Write number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int evenN = 2;
+
+if(n > 1)
+{
+    while(evenN <= n)
+    {
+        Console.Write(evenN + " ");
+        evenN = evenN + 2;
+    }
+}
