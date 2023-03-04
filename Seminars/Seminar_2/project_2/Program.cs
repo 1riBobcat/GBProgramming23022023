86776d7be8fe583386635a1d4b74f2ffbab6460f
1riBobcat <irisha.bobcat@gmail.com>
2023-03-04 19:16:31 +0300

    2_seminars

---
+++ Seminars/Seminar_2/project_2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа 
+//или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+
+
+
+
+
+  Console.Write("Write Number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+string b = Convert.ToString(a);
+if (b.Length > 2){
+  Console.WriteLine("three digit -> " + b[2]);
+}
+else {
+  Console.WriteLine("-> no third digit");
+}
---
+++ Seminars/Seminar_2/project_3/Program.cs
@@ -0,0 +1,23 @@
+﻿//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+//и проверяет, является ли этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+
+Console.WriteLine("Write number of the day of the week: ");
+int day = Convert.ToInt32(Console.ReadLine());
+
+    void CheckingTheDayOfTheWeek (int day) {
+    if (day < 1 || day > 7) {
+        Console.WriteLine("there is no such day of the week. Write again.");
+    }
+        else 
+            if (day == 6 || day == 7)  {
+            Console.WriteLine("it's a day off -> да");
+  }
+   
+        else 
+            Console.WriteLine("it's not a day off -> нет");
+    }
+CheckingTheDayOfTheWeek(day);
