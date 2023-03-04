958b6973ce721abcf69ec1c32d2e7ede427c8672
1riBobcat <irisha.bobcat@gmail.com>
2023-03-04 20:09:46 +0300

    3_seminars

---
+++ Seminars/Seminar_3/project_1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 19: Напишите программу, которая принимает на вход пятизначное число 
+//и проверяет, является ли оно палиндромом.
+
+// 14212 -> нет
+// 23432 -> да
+// 12821 -> да
+
+
+Console.Write("Write number: ");
+string? n = Console.ReadLine();
+
+void CheckingNumber(string n){
+  if (n[0]==n[4] || n[1]==n[3]){
+    Console.WriteLine($"You number: {n} - palindrome.");
+  }
+  else Console.WriteLine($"You number: {n} - not palindrome.");
+}
+
+if (n!.Length == 5){
+  CheckingNumber(n);
+}
+else Console.WriteLine($"Write rite number");
+
---
+++ Seminars/Seminar_3/projekt_3/Program.cs
@@ -0,0 +1,30 @@
+﻿//Задача 23: Напишите программу, которая принимает на вход число (N) 
+//и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+
+Console.Write("Write namber: ");
+int root = Convert.ToInt32(Console.ReadLine());
+
+void Cube(int[] root){
+  int counter = 0;
+  int length = root.Length;
+  while (counter <  length){
+    root[counter] = Convert.ToInt32(Math.Pow(counter, 3));
+    counter++;
+  }
+}
+
+void PrintArry(int[] coll){
+  int count = coll.Length;
+  int index = 0;
+  while(index < count){
+    Console.Write(coll[index]+ " ");
+    index++;
+  }
+} 
+
+int[] cube = new int[root+1];
+Cube(cube);
+PrintArry(cube);
